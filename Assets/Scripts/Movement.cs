98e2380348863dcad6fb314a3e5a1ef14940fc6a
stkurz <stkurz@stud.hs-heilbronn.de>
2023-10-14 17:01:13 +0200

    Basic left right movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,15 +8,38 @@ using UnityEngine.Serialization;
 public class Movement : MonoBehaviour
 {
     private const float _mvspeed = 5f;
+    private const float _jmpHight = 1f;
 
-    [SerializeField] private InputAction _playerControls;
     [SerializeField] private Rigidbody _rb;
 
     private Vector2 _movDir = Vector2.zero;
-    
-    private void OnEnable() => _playerControls.Enable();
 
-    private void OnDisable() => _playerControls.Disable();
+    private PlayerControlls _playerControlls;
+    
+    private InputAction _move;
+    private InputAction _fire;
+
+    private void Awake()
+    {
+        _playerControlls = new();
+    }
+
+    private void OnEnable()
+    {
+        _move = _playerControlls.Player.Move;
+        _move.Enable();
+
+        _fire = _playerControlls.Player.Fire;
+        _fire.Enable();
+        _fire.performed += Fire;
+    }
+
+    private void OnDisable()
+    {
+        _move.Disable();
+        _fire.Disable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,12 +49,17 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _movDir = _playerControls.ReadValue<Vector2>();
+        _movDir = _move.ReadValue<Vector2>();
     }
 
     private void FixedUpdate()
     {
         var velocity = _rb.velocity;
-        _rb.velocity = new(_movDir.x * _mvspeed,velocity.y,velocity.z);
+        _rb.velocity = new((-_movDir.x) * _mvspeed,velocity.y,velocity.z);
+    }
+
+    private void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("FIRED");
     }
 }
