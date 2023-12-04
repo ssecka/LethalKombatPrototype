32911cca2a8e32e1c4aa0042598085de5188309c
DeavC <72741224+DeavC@users.noreply.github.com>
2023-12-04 19:14:05 +0100

    more stuff

--- Assets/Scripts/Networking/NetworkCharacterControllerPrototypeCustom.cs
+++ Assets/Scripts/Networking/NetworkCharacterControllerPrototypeCustom.cs
@@ -92,39 +92,43 @@ public class NetworkCharacterControllerPrototypeCustom : NetworkTransform
     public virtual void Move(Vector3 direction)
     {
         Debug.Log($"Move [x:{direction.x} | y: {direction.y} | z: {direction.z}]");
-        
+
         var deltaTime = Runner.DeltaTime;
-        var previousPos = transform.position;
-        var moveVelocity = Velocity;
 
-        direction = direction.normalized;
+        var speed = 1 * deltaTime;
+        
+        if (direction.x > 0)
+            Controller.Move(new(speed, 0, 0));
+        if (direction.x < 0)
+            Controller.Move(new(-speed, 0, 0));
 
-        if (IsGrounded && moveVelocity.y < 0)
-        {
-            moveVelocity.y = 0f;
-        }
 
-        moveVelocity.y += gravity * Runner.DeltaTime;
-
-        var horizontalVel = default(Vector3);
-        horizontalVel.x = moveVelocity.x;
-        horizontalVel.z = moveVelocity.z;
-
-        if (direction == default)
-        {
-            horizontalVel = Vector3.Lerp(horizontalVel, default, braking * deltaTime);
-        }
-        else
-        {
-            horizontalVel = Vector3.ClampMagnitude(horizontalVel + direction * acceleration * deltaTime, maxSpeed);
-        }
-
-        moveVelocity.x = horizontalVel.x;
-        moveVelocity.z = horizontalVel.z;
-
-        Controller.Move(moveVelocity * deltaTime);
-
-        Velocity = (transform.position - previousPos) * Runner.Simulation.Config.TickRate;
+        // if (IsGrounded && moveVelocity.y < 0)
+        // {
+        //     moveVelocity.y = 0f;
+        // }
+        //
+        // moveVelocity.y += gravity * Runner.DeltaTime;
+        //
+        // var horizontalVel = default(Vector3);
+        // horizontalVel.x = moveVelocity.x;
+        // horizontalVel.z = moveVelocity.z;
+        //
+        // if (direction == default)
+        // {
+        //     horizontalVel = Vector3.Lerp(horizontalVel, default, braking * deltaTime);
+        // }
+        // else
+        // {
+        //     horizontalVel = Vector3.ClampMagnitude(horizontalVel + direction * acceleration * deltaTime, maxSpeed);
+        // }
+        //
+        // moveVelocity.x = horizontalVel.x;
+        // moveVelocity.z = horizontalVel.z;
+        //
+        // Controller.Move(moveVelocity * deltaTime);
+        //
+        // Velocity = (transform.position - previousPos) * Runner.Simulation.Config.TickRate;
         IsGrounded = Controller.isGrounded;
     }
 }
