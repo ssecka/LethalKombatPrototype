b2ca0bb68e4f5d2d71d5dd6b255bf0be94d6e96d
Telmo-Santos <telmo2811@gmail.com>
2024-01-13 19:58:49 +0100

    Further Rounds developments

--- Assets/Scripts/HP/HPHandler.cs
+++ Assets/Scripts/HP/HPHandler.cs
@@ -5,26 +5,22 @@ using Fusion;
 public class HPHandler : NetworkBehaviour
 {
     private const short STARTING_HP = 1_000;
-    public short _playerNum;
     [Networked(OnChanged = nameof(OnHPChanged))]
     
     private short HP { get; set; } // We only use short for performance
-    private short Round { get; set; }
+
+    private short Round = 0;
     
     [Networked(OnChanged = nameof(OnStateChanged)), HideInInspector]
     public bool isDead { get; set; }
     private bool isInit = false;
 
     public HealthBarScript healthBarScript;
-    public GameObject hitEffect;
-    public GameObject blockEffect;
-    public Transform hitPoint;
     
     private void Start()
     {
         healthBarScript = GetComponent<HealthBarScript>();
-        healthBarScript.SetMaxHealth(STARTING_HP,transform.name);
-        Round = 0;
+        healthBarScript.SetMaxHealth(STARTING_HP, transform.name);
         HP = STARTING_HP;
         isDead = false;
     }
@@ -33,24 +29,11 @@ public class HPHandler : NetworkBehaviour
     public void OnHitTaken(short amount, bool isBlocking = false)
     {
         if (isDead) return; // -> we deadge
-        
-        Runner.Spawn(hitEffect, hitPoint.position);
-        
+
         if (isBlocking)
         {
             amount /= 10;
             print("BLOCKED!");
-            Runner.Spawn(blockEffect, hitPoint.position);
-            
-            int yRotation = Convert.ToInt32(transform.eulerAngles.y);
-            if (yRotation == 90)
-            {
-                transform.Translate(Vector3.back * .5f);
-            }
-            else
-            {
-                transform.Translate(Vector3.forward * .5f);
-            }
         }
         
         HP -= amount;
@@ -61,29 +44,35 @@ public class HPHandler : NetworkBehaviour
         {
             Round++;
             isDead = true;
-            
             Debug.Log($"{transform.name} is dead.");
-
+            
         }
+    }
+
+    public void ResetHp()
+    {
+        HP = STARTING_HP;
     }
 
     private static void OnHPChanged(Changed<HPHandler> changed)
     {
-        //healthbBarScript.SetHealth(changed.Behaviour.HP, transform.name);
         Debug.Log($"OnHPChanged value {changed.Behaviour.HP}");
         changed.Behaviour.healthBarScript.SetHealth(changed.Behaviour.HP, changed.Behaviour.transform.name);
         if (changed.Behaviour.HP <= 0)
         {
             changed.Behaviour.healthBarScript.SetRound(changed.Behaviour.Round,changed.Behaviour.transform.name);
-            changed.Behaviour.healthBarScript.SetMaxHealth(1000, changed.Behaviour.transform.name);
+            changed.Behaviour.healthBarScript.ResetHealthBarHost(STARTING_HP); 
+            changed.Behaviour.healthBarScript.ResetHealthBarClient(STARTING_HP);
+            
+            if (changed.Behaviour.Round <= 2)
+            {
+                changed.Behaviour.ResetHp();
+            }
         }
-
     }
 
     private static void OnStateChanged(Changed<HPHandler> changed)
     {
         Debug.Log($"OnStateChanged value {changed.Behaviour.isDead}");
-
     }
-    
 }
--- Assets/Scripts/HealthBarScript.cs
+++ Assets/Scripts/HealthBarScript.cs
@@ -5,6 +5,7 @@ public class HealthBarScript : MonoBehaviour
 {
 
     private GameObject _healthBarObject;
+
     public Slider HealthSlider;
     public Image Fill;
     private Gradient _gradient;
@@ -109,5 +110,77 @@ public class HealthBarScript : MonoBehaviour
                 Fill.color = Color.yellow;
             }     
         }
+    }
+
+    public void ResetHealthBarHost(int health)
+    {
+        _gradient ??= new();
+
+        var colors = new GradientColorKey[3];
+        colors[0] = new(Color.red, 0.0f);
+        colors[1] = new(Color.yellow, 0.5f);
+        colors[2] = new(Color.green, 1.0f);
+
+        var alphas = new GradientAlphaKey[3];
+        alphas[0] = new(1.0f, 0);
+        alphas[1] = new(1.0f, 0.5f);
+        alphas[2] = new(1.0f, 1f);
+
+        _gradient.SetKeys(colors, alphas);
+        
+        _healthBarObject = GameObject.Find("Canvas").transform.GetChild(0).gameObject;
+        
+        HealthSlider = _healthBarObject.GetComponent<Slider>();
+        Fill = _healthBarObject.transform.GetChild(0).GetComponent<Image>();
+
+        if (HealthSlider == null || Fill == null)
+        {
+            // Handle the case where either HealthSlider or Fill is still null
+            Debug.LogError("HealthSlider or Fill is not found.");
+            return;
+        }
+
+        GeneralFunctions.PrintDebugStatement(HealthSlider);
+
+        HealthSlider.maxValue = health;
+        HealthSlider.value = health;
+
+        Fill.color = _gradient.Evaluate(1f);
+    }
+    
+    public void ResetHealthBarClient(int health)
+    {
+        _gradient ??= new();
+
+        var colors = new GradientColorKey[3];
+        colors[0] = new(Color.red, 0.0f);
+        colors[1] = new(Color.yellow, 0.5f);
+        colors[2] = new(Color.green, 1.0f);
+
+        var alphas = new GradientAlphaKey[3];
+        alphas[0] = new(1.0f, 0);
+        alphas[1] = new(1.0f, 0.5f);
+        alphas[2] = new(1.0f, 1f);
+
+        _gradient.SetKeys(colors, alphas);
+        
+        _healthBarObject = GameObject.Find("Canvas").transform.GetChild(1).gameObject;
+        
+        HealthSlider = _healthBarObject.GetComponent<Slider>();
+        Fill = _healthBarObject.transform.GetChild(0).GetComponent<Image>();
+
+        if (HealthSlider == null || Fill == null)
+        {
+            // Handle the case where either HealthSlider or Fill is still null
+            Debug.LogError("HealthSlider or Fill is not found.");
+            return;
+        }
+
+        GeneralFunctions.PrintDebugStatement(HealthSlider);
+
+        HealthSlider.maxValue = health;
+        HealthSlider.value = health;
+
+        Fill.color = _gradient.Evaluate(1f);
     }
 }
