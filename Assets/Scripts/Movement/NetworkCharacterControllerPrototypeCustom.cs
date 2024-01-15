8be3abba9c3b68967cef33a3dccc332678f359fd
unknown <ssecka@stud.hs-heilbronn.de>
2024-01-15 15:15:40 +0100

    Adjusted FireBall height to make it jumpable

--- Assets/Scripts/Movement/NetworkCharacterControllerPrototypeCustom.cs
+++ Assets/Scripts/Movement/NetworkCharacterControllerPrototypeCustom.cs
@@ -584,7 +584,7 @@ public class NetworkCharacterControllerPrototypeCustom : NetworkTransform
         var rotation = transform.rotation;
         var pos = transform.position;
         float addedX = Math.Abs(transform.rotation.eulerAngles.y - 90) < 0.002 ? 0.75f : -0.75f;
-        pos = new(pos.x + addedX, pos.y + 0.925f, pos.z);
+        pos = new(pos.x + addedX, pos.y-.15f + 0.925f, pos.z);
 
         Runner.Spawn(fireBall, pos, rotation, Object.InputAuthority, (runner, spawnedFireball) =>
         {
